e8887b4413323022e52f5e7af3788b42f71bafb1
Michael Thiele-Maas <michael@Michaels-MacBook-Pro.local>
2015-09-09 14:36:10 +0200

    Prepared client side network script to connect to the main server and introduce itself as a player

--- sources/mainServer/public/js/network.js
+++ sources/mainServer/public/js/network.js
@@ -1,7 +1,11 @@
 var network = (function(){
   return {
     init : function() {
-      
+      var socket = io.connect('http://localhost:4000');
+      socket.emit ('introduction', 'player');
+      socket.on ('disconnect', function(){
+        console.log("Disconnected from the server. Maybe reloading the page will help.");
+      });
     }
   }
 })();
