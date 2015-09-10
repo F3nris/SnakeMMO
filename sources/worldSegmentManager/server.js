a2d77949bde8fae4d43d7e748ed9175a1bb9db49
Michael Thiele-Maas <michael@Michaels-MacBook-Pro.local>
2015-09-10 11:18:01 +0200

    Added funtionality to inform mainserver about ip and port of the segment manager (so he can later give them to players

--- sources/worldSegmentManager/server.js
+++ sources/worldSegmentManager/server.js
@@ -1,15 +1,15 @@
 var io = require('socket.io')();
 var ioClient = require('socket.io-client')('http://localhost:4000');
 var findPort = require('find-port');
+var ip = require('my-local-ip');
 
 var config = require('./config.js');
 
 findPort(config.IO_PORT_MIN, config.IO_PORT_MAX, function(ports) {
   console.log("Starting SegmentManager on Port "+ports[0]);
   io.listen(ports[0]);
+  ioClient.emit("introduction", { 'role': 'segmentmanager', 'ip': ip(), 'port': ports[0]});
 });
-
-ioClient.emit("introduction", "segmentmanager");
 
 // Initialize its own server
 io.on('connection',function(socket){
