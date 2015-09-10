adf32a7cafd67e9102c4e5674aa7fd388fcffa22
Michael Thiele-Maas <michael@Michaels-MacBook-Pro.local>
2015-09-10 11:19:01 +0200

    Updated player-side network config to fit to the changes made for the worldsegmentmanager

--- sources/mainServer/public/js/network.js
+++ sources/mainServer/public/js/network.js
@@ -2,7 +2,7 @@ var network = (function(){
   return {
     init : function() {
       var socket = io.connect('http://localhost:4000');
-      socket.emit ('introduction', 'player');
+      socket.emit ('introduction', {'role' : 'player'});
       socket.on ('disconnect', function(){
         console.log("Disconnected from the server. Maybe reloading the page will help.");
       });
